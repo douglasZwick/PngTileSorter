9fddd595c5a8f2475d49c65c95ad59afce79145b
modusponens <modusponens@gmail.com>
2020-10-05 23:33:54 -0700

    Added project

---
+++ PngTileSorter/BitmapPixelGrid.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PngTileSorter
+{
+  public class BitmapPixelGrid
+  {
+    public int m_Width { get; private set; }
+    public int m_Height { get; private set; }
+
+    byte[] m_Pixels;
+    int m_Stride;
+
+    public BitmapPixelGrid(BitmapImage bitmap)
+    {
+      m_Width = bitmap.PixelWidth;
+      m_Height = bitmap.PixelHeight;
+      m_Pixels = new byte[m_Width * m_Height * 4];
+      m_Stride = m_Width * 4;
+
+      bitmap.CopyPixels(m_Pixels, m_Stride, 0);
+    }
+
+    public Color this[int i]
+    {
+      get
+      {
+        i *= 4;
+        var r = m_Pixels[i + 0];
+        var g = m_Pixels[i + 1];
+        var b = m_Pixels[i + 2];
+        var a = m_Pixels[i + 3];
+
+        return new Color() { R = r, G = g, B = b, A = a };
+      }
+
+      set
+      {
+        i *= 4;
+        m_Pixels[i + 0] = value.R;
+        m_Pixels[i + 1] = value.G;
+        m_Pixels[i + 2] = value.B;
+        m_Pixels[i + 3] = value.A;
+      }
+    }
+
+    public Color this[int x, int y]
+    {
+      get
+      {
+        var index = y * m_Stride + x * 4;
+        var r = m_Pixels[index + 0];
+        var g = m_Pixels[index + 1];
+        var b = m_Pixels[index + 2];
+        var a = m_Pixels[index + 3];
+
+        return new Color() { R = r, G = g, B = b, A = a };
+      }
+
+      set
+      {
+        var index = y * m_Stride + x * 4;
+        m_Pixels[index + 0] = value.R;
+        m_Pixels[index + 1] = value.G;
+        m_Pixels[index + 2] = value.B;
+        m_Pixels[index + 3] = value.A;
+      }
+    }
+  }
+}
---
+++ PngTileSorter/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PngTileSorter
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private int m_TileWidth_ = 8;
+    public int m_TileWidth
+    {
+      get { return m_TileWidth_; }
+      set
+      {
+        var old = m_TileWidth_;
+        m_TileWidth_ = value;
+
+        if (value != old)
+          OnPropertyChanged("m_TileWidth");
+      }
+    }
+
+    private int m_TileHeight_ = 8;
+    public int m_TileHeight
+    {
+      get { return m_TileHeight_; }
+      set
+      {
+        var old = m_TileHeight_;
+        m_TileHeight_ = value;
+
+        if (value != old)
+          OnPropertyChanged("m_TileHeight");
+      }
+    }
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+    {
+      var dialog = new OpenFileDialog();
+      dialog.Filter = "PNG Images|*.png";
+      dialog.DefaultExt = ".png";
+      dialog.InitialDirectory = Environment.CurrentDirectory;
+
+      if (dialog.ShowDialog() == true)
+      {
+        var path = dialog.FileName;
+        m_FilePath.Text = path;
+        var fileUri = new Uri(path);
+        var bitmap = new BitmapImage(fileUri);
+        m_SourceBackground.Width = bitmap.Width;
+        m_SourceBackground.Height = bitmap.Height;
+        m_SourceImage.Width = bitmap.Width;
+        m_SourceImage.Height = bitmap.Height;
+        m_SourceImage.Source = bitmap;
+        m_OutputBackground.Width = bitmap.Width;
+        m_OutputBackground.Height = bitmap.Height;
+        m_OutputImage.Width = bitmap.Width;
+        m_OutputImage.Height = bitmap.Height;
+
+        var pixels = new BitmapPixelGrid(bitmap);
+
+        var cols = bitmap.PixelWidth / m_TileWidth;
+        var rows = bitmap.PixelHeight / m_TileHeight;
+        var tileCount = cols * rows;
+        var weights = new float[tileCount];
+
+        for (var row = 0; row < rows; ++row)
+        {
+          for (var col = 0; col < cols; ++col)
+          {
+            var weightIndex = row * cols + col;
+
+            for (var j = 0; j < m_TileHeight; ++j)
+            {
+              for (var i = 0; i < m_TileWidth; ++i)
+              {
+                var x = i + col * m_TileWidth;
+                var y = j + row * m_TileHeight;
+                var color = pixels[x, y];
+                weights[weightIndex] += color.A / 255.0f;
+              }
+            }
+
+            weights[weightIndex] /= tileCount;
+          }
+        }
+
+        // Now sort the weights array, repeatedly finding
+        // the smallest item and removing it, while also:
+        //   - copying that tile to a writeable bitmap
+        //   - recording the index and weight for logging
+
+        // Then display the new bitmap in the other image
+        // as a preview and enable the button that allows
+        // the sorted image to be written to file
+      }
+    }
+
+
+    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
